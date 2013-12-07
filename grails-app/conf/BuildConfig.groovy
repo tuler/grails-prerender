2c10eb51c169fc1f35f106873ce26b0e9c6627e6
Burt Beckwith <burt@burtbeckwith.com>
2013-12-07 03:55:49 -0500

    cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,22 +1,22 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = 'target'
 
 grails.project.dependency.resolution = {
-	inherits("global") {
-	}
-	log "warn"
-	legacyResolve false
+
+	inherits 'global'
+	log 'warn'
+
 	repositories {
 		grailsCentral()
+		mavenLocal()
 		mavenCentral()
 	}
+
 	dependencies {
 		runtime 'com.github.greengerong:prerender-java:1.0'
 	}
 
 	plugins {
-		build(":tomcat:$grailsVersion", ":release:2.2.1", ":rest-client-builder:1.0.3") {
+		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
 			export = false
 		}
 		compile ':webxml:1.4.1'
