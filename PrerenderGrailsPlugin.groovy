de7924f776e8d4e51f734085d682717b3b4d625e
Burt Beckwith <burt@burtbeckwith.com>
2013-12-07 03:50:21 -0500

    dropped the minimum Grails version to 2.0

--- PrerenderGrailsPlugin.groovy
+++ PrerenderGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class PrerenderGrailsPlugin {
 	// the plugin version
 	def version = "1.0.0"
 	// the version or versions of Grails the plugin is designed for
-	def grailsVersion = "2.2 > *"
+	def grailsVersion = "2.0 > *"
 	// resources that are excluded from plugin packaging
 	def pluginExcludes = [
 		"grails-app/views/error.gsp"
